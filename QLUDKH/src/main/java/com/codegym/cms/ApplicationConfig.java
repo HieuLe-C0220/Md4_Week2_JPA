1e7ee778a95dea4a4f40761ad8013fec2d9cd9ed
Hieu.Le <letrunghieu2602@gmail.com>
2020-06-23 16:44:59 +0700

    done

--- QLUDKH/src/main/java/com/codegym/cms/ApplicationConfig.java
+++ QLUDKH/src/main/java/com/codegym/cms/ApplicationConfig.java
@@ -45,7 +45,7 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
     public SpringResourceTemplateResolver templateResolver(){
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/views");
+        templateResolver.setPrefix("/WEB-INF/views/customer");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
         return templateResolver;
--- QLUDKH/src/main/java/com/codegym/cms/controller/CustomerController.java
+++ QLUDKH/src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,14 +20,14 @@ public class CustomerController {
 
     @GetMapping("/create-customer")
     public ModelAndView showCreateForm() {
-        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        ModelAndView modelAndView = new ModelAndView("/create");
         modelAndView.addObject("customer",new Customer());
         return modelAndView;
     }
     @PostMapping("/create-customer")
     public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
         customerService.save(customer);
-        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        ModelAndView modelAndView = new ModelAndView("/create");
         modelAndView.addObject("customer", new Customer());
         modelAndView.addObject("message", "New customer created successfully");
         return modelAndView;
@@ -34,8 +35,47 @@ public class CustomerController {
     @GetMapping("/customer")
     public ModelAndView listCustomer() {
         List<Customer> customers = customerService.findAll();
-        ModelAndView modelAndView = new ModelAndView("/customer/list");
-        modelAndView.addObject("customer",customers);
+        ModelAndView modelAndView = new ModelAndView("/list");
+        modelAndView.addObject("customers",customers);
         return modelAndView;
+    }
+    @GetMapping("/edit-customer/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+        ModelAndView modelAndView;
+        if (customer!=null) {
+            modelAndView = new ModelAndView("/edit");
+            modelAndView.addObject("customer",customer);
+        } else {
+            modelAndView = new ModelAndView("/error.404");
+        }
+        return modelAndView;
+    }
+    @PostMapping("/edit-customer")
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+        ModelAndView modelAndView;
+        if(customer != null) {
+            modelAndView = new ModelAndView("/delete");
+            modelAndView.addObject("customer", customer);
+
+        }else {
+            modelAndView = new ModelAndView("/error.404");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/delete-customer")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.remove(customer.getId());
+        return "redirect:customer";
     }
 }
